690cb95098c129a23fbb717c576fdca774e37bc9
heretere <heirteir@protonmail.com>
2021-10-21 11:02:58 -0400

    Initial Rework Commit

---
+++ settings.gradle.kts
@@ -0,0 +1,14 @@
+pluginManagement {
+    repositories {
+        mavenLocal()
+        gradlePluginPortal()
+        maven { url = uri("https://oss.sonatype.org/content/repositories/snapshots/") }
+    }
+}
+
+rootProject.name = "hdl"
+
+include(":common")
+include(":plugin")
+include(":core")
+include(":spigot-plugin")
